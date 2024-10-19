8695264ba9eaf0a82d9f94be147378885d4e087f
Codebolt <dathphan@ucsc.edu>
2024-10-18 22:43:44 -0700

    Step 1 Completed

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import "./style.css";
 
+// APP TITLES
 const APP_NAME = "CMPM 121 Demo 2";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const title = document.createElement("h1");
+title.textContent = "[APP_TITLE]"
+app.append(title);
+
+// CANVAS
+const canvas: HTMLCanvasElement = document.createElement("canvas") as HTMLCanvasElement;
+app.append(canvas)
+canvas.width = 256;
+canvas.height = 256;
