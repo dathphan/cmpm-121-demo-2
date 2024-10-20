c62f4fb9ca98a9ca64e0ca847e2d55774fd37fff
Codebolt <dathphan@ucsc.edu>
2024-10-20 13:26:32 -0700

    Step 8 Complete

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,38 @@ interface Point {
 }
 
 interface Command {
+    cleanCopy(): Command;
     display(ctx: CanvasRenderingContext2D);
+    preview(ctx: CanvasRenderingContext2D, point: Point);   // I realized I wasn't suppose to do this, but I think the code is cleaner as a result
+    mouseDown(ctx: CanvasRenderingContext2D, point: Point);
+    drag(ctx: CanvasRenderingContext2D, point: Point);
+    mouseUp(ctx: CanvasRenderingContext2D, point: Point);
 }
 
 class Line implements Command {
     points: Point[] = []
-    width: number
+    size: number
 
-    constructor(point: Point, width: number) {
-        this.points = [point]
-        this.width = width;
+    cleanCopy(): Command {
+        return new Line(this.size);
     }
 
-    push(point: Point) {
+    constructor(size: number) {
+        this.size = size;
+    }
+
+    mouseDown(ctx: CanvasRenderingContext2D, point: Point){
+        this.drag(ctx, point);
+    };
+    drag(ctx: CanvasRenderingContext2D, point: Point) {
         this.points.push(point);
         dispatchEvent(drawingChanged);
+    }
+    mouseUp(_ctx: CanvasRenderingContext2D, _point: Point){};
+
+    preview(ctx: CanvasRenderingContext2D, point: Point) {
+        ctx.font = this.size * 8 + "px monospace";
+        ctx.fillText("+", point.x - this.size * 2, point.y + this.size * 2);
     }
 
     display(ctx: CanvasRenderingContext2D) {
@@ -35,7 +52,7 @@ class Line implements Command {
     start(ctx: CanvasRenderingContext2D, point: Point){
         ctx.beginPath();
         ctx.strokeStyle = lineColor;
-        ctx.lineWidth = this.width;
+        ctx.lineWidth = this.size;
         ctx.moveTo(point.x, point.y);
     }
 
@@ -49,21 +66,36 @@ class Line implements Command {
     }
 }
 
-class Preview implements Command{
+class Sticker implements Command {
+    sticker: string = ""
     point: Point = Point(-1, -1);
-    active: boolean = false;
+    size: number = 6;
 
-    move(point: Point) {
+    cleanCopy(): Command {
+        return new Sticker(this.sticker);
+    }
+
+    constructor(sticker: string) {
+        this.sticker = sticker;
+    }
+
+    mouseDown(ctx: CanvasRenderingContext2D, point: Point){
+        this.drag(ctx, point);
+    };
+    drag(ctx: CanvasRenderingContext2D, point: Point) {
         this.point = point;
-        this.active = point.x >= 0;
         dispatchEvent(drawingChanged);
+    }
+    mouseUp(ctx: CanvasRenderingContext2D, point: Point){};
+    
+    preview(ctx: CanvasRenderingContext2D, point: Point) {
+        ctx.font = this.size * 8 + "px monospace";
+        ctx.fillText(this.sticker, point.x - this.size * 5.3, point.y + this.size * 2);
     }
 
     display(ctx: CanvasRenderingContext2D) {
-        if (!preview.active) return;
-        
-        ctx.font = lineWidth * 8 + "px monospace";
-        ctx.fillText("+", this.point.x - lineWidth * 2, this.point.y + lineWidth * 2);
+        if (this.point.x < 0) return;
+        this.preview(ctx, this.point);
     }
 }
 
@@ -71,15 +103,17 @@ function Point (x: number, y: number) : Point {
     return { x: x, y: y}
 }
 
+let currentTool: Command;
+
 let displayActions: Command[] = []
 let redoActions: Command[] = []
-let preview: Preview = new Preview();
+let canvasPos: Point = Point(-1, -1);
 const drawingChanged: Event = new Event("drawing-changed");
 const toolMoved: Event = new Event("tool-moved");
 
-const lineWidths: number[] = [4, 6, 8]
-let lineWidthButtons: HTMLButtonElement[] = [];
-let lineWidth: number = 4;
+const brushSizes: number[] = [4, 6, 8]
+const stickers: string[] = ["🐯", "🐻", "🦝"]
+let toolButtons: HTMLButtonElement[] = [];
 const lineColor: string = "black";
 
 let isDrawing: boolean = false;
@@ -105,22 +139,23 @@ const brushHeader: HTMLDivElement = document.createElement("div");
 brushHeader.innerHTML = "Brushes";
 app.append(brushHeader);
 
-lineWidths.forEach(width => {
-    const widthButton: HTMLButtonElement = document.createElement("button");
-    widthButton.innerHTML = "[" + width + "]";
-    lineWidthButtons.push(widthButton);
-    app.append(widthButton);
+brushSizes.forEach(size => {
+    const sizeButton: HTMLButtonElement = document.createElement("button");
+    sizeButton.innerHTML = "[" + size + "]";
+    toolButtons.push(sizeButton);
+    app.append(sizeButton);
 
-    widthButton.addEventListener("click", () => {
-        lineWidth = width;
-        lineWidthButtons.forEach(button => {
+    sizeButton.addEventListener("click", () => {
+        currentTool = new Line(size);
+
+        toolButtons.forEach(button => {
             button.classList.remove("selectedTool");
         });
-        widthButton.classList.add("selectedTool");
+        sizeButton.classList.add("selectedTool");
     })
 });
 
-lineWidthButtons.at(0)?.click();
+toolButtons.at(0)?.click();
 
 const controlHeader: HTMLDivElement = document.createElement("div");
 controlHeader.innerHTML = "Controls";
@@ -153,6 +188,13 @@ redoButton.addEventListener("click", () => {
     redo();
 });
 
+const stickerHeader: HTMLDivElement = document.createElement("div");
+stickerHeader.innerHTML = "Stickers";
+app.append(stickerHeader);
+
+stickers.forEach(sticker => {
+    addSticker(sticker);
+});
 
 const context: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
 
@@ -161,6 +203,9 @@ function drawCanvas(ctx: CanvasRenderingContext2D) {
     displayActions.forEach(actions => {
         actions.display(ctx);
     });
+
+    if (canvasPos.x < 0) return;
+    currentTool.preview(ctx, canvasPos);
 }
 
 function clear(ctx: CanvasRenderingContext2D) {
@@ -185,37 +230,57 @@ function redo() {
     dispatchEvent(drawingChanged);
 }
 
+function addSticker(sticker: string) {
+    const stickerButton: HTMLButtonElement = document.createElement("button");
+    stickerButton.innerHTML = "[" + sticker + "]";
+    toolButtons.push(stickerButton);
+    app.append(stickerButton);
+
+    stickerButton.addEventListener("click", () => {
+        currentTool = new Sticker(sticker);
+
+        toolButtons.forEach(button => {
+            button.classList.remove("selectedTool");
+        });
+        stickerButton.classList.add("selectedTool");
+    })
+}
+
 // Canvas Inputs
 canvas.addEventListener("mousedown", (e) => {
     isDrawing = true;
-    displayActions.push(new Line(Point(e.offsetX, e.offsetY), lineWidth))
+
+    displayActions.push(currentTool);
+    currentTool.mouseDown(context, Point(e.offsetX, e.offsetY));
     redoActions = [];
 });
 
-canvas.addEventListener("mousemove", (e) => {
-
+window.addEventListener("mouseup", (e) => {
     if (isDrawing) {
+        currentTool = currentTool.cleanCopy();
         let command: Command = displayActions.at(-1) as Command;
-        if (command instanceof Line) {
-            (command as Line).push(Point(e.offsetX, e.offsetY));
-        }
+        command.mouseUp(context, Point(e.offsetX, e.offsetY));
+        isDrawing = false;
+    }
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    canvasPos = Point(e.offsetX, e.offsetY);
+    if (isDrawing) {
+        currentTool.drag(context, canvasPos);
     }
 
-    preview.move(Point(e.offsetX, e.offsetY));
     dispatchEvent(toolMoved);
 });
 
-window.addEventListener("mouseup", () => {
-    if (!isDrawing) return;
-    isDrawing = false;
-});
-
 canvas.addEventListener("mouseenter", () => {
-    preview.move(Point(-1, -1))
+    canvasPos = Point(-1, -1);
+    dispatchEvent(toolMoved);
 });
 
 canvas.addEventListener("mouseout", () => {
-    preview.move(Point(-1, -1))
+    canvasPos = Point(-1, -1);
+    dispatchEvent(toolMoved);
 });
 
 window.addEventListener("drawing-changed", () => {
@@ -224,5 +289,6 @@ window.addEventListener("drawing-changed", () => {
 });
 
 window.addEventListener("tool-moved", () => {
-    preview.display(context);
+    clearCanvas(context);
+    drawCanvas(context);
 });
